4770c5ef6aced4dfb24dfd92f812f519d10601f6
omarmontoys <omarmontoyaromero@gmail.com>
2023-03-06 19:50:54 -0600

    :+1:

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <v-form action="#">
+      
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+</style>
