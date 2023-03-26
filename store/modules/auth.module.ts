a8b693bc928e14d99bbbce74b52c43c5cab6b612
omarmontoys <omarmontoyaromero@gmail.com>
2023-03-26 02:04:04 -0600

    :construction:
    Se configuraron los modules y services de las mutaciones y querys

---
+++ store/modules/auth.module.ts
@@ -0,0 +1,80 @@
+import { Action, Module, Mutation, VuexModule } from "vuex-module-decorators";
+import { Auth, CreateUserInput, LoginInput, User } from "~/gql/graphql";
+import authService from "~/services/auth.service";
+
+import AuthService from "~/services/auth.service";
+
+@Module({ namespaced: true })
+class AuthModule extends VuexModule {
+  public user?: User = undefined;
+  public loadingLoginStatus = false;
+  public loadingRegisterStatus = false;
+  @Mutation
+  public removeCookies() {
+    window.$nuxt.$cookies.remove("token");
+    window.$nuxt.$router.push("/");
+  }
+  @Action({ rawError: true })
+  logOut(): void {
+    this.context.commit("removeCookies");
+  }
+  @Action
+  async login(data: LoginInput) {
+    this.context.commit("loadingLogin", true);
+    return await AuthService.login(data)
+      .then((auth: Auth) => {
+        console.log(auth);
+        this.context.commit("loginSuccess", auth);
+        this.context.commit("loadingLogin", false);
+        window.$nuxt.$router.push("./PagPrin/Principal");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  @Action
+  async registerUser(data: CreateUserInput) {
+    this.context.commit("loadingRegister", true);
+    return await AuthService.registerUser(data)
+      .then(async () => {
+        return await AuthService.login({
+          email: data.email,
+          password: data.password,
+        })
+          .then((auth: Auth) => {
+            console.log(auth);
+            this.context.commit("loginSuccess", auth);
+            this.context.commit("loadingRegister", false);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        window.$nuxt.$router.push("/");
+      });
+  }
+  @Mutation
+  public loginSuccess(auth: Auth): void {
+    console.log(auth);
+    window.$nuxt.$cookies.set("token", auth.token, {
+      path: "/",
+    });
+  }
+  @Mutation
+  public loadingLogin(status: boolean) {
+    this.loadingLoginStatus = status;
+  }
+  @Mutation
+  public loadingRegister(status: boolean) {
+    this.loadingRegisterStatus = status;
+  }
+  get isLoadingLogin(): boolean {
+    return this.loadingLoginStatus;
+  }
+  get isLoadingRegister(): boolean {
+    return this.loadingRegisterStatus;
+  }
+}
+export default AuthModule;
