49a8a3e50ef50f02bf5d0fd2b8b6e8f6d5f7701d
omarmontoys <omarmontoyaromero@gmail.com>
2023-06-19 09:13:38 -0600

    :sparkles:

--- pages/PagPrin/Principal.vue
+++ pages/PagPrin/Principal.vue
@@ -194,9 +194,6 @@
                 </v-toolbar>
               </template>
               <template v-slot:item.actions="{ item }">
-                <v-icon small class="mr-2" @click="editItem(item)">
-                  mdi-pencil
-                </v-icon>
                 <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
               </template>
             </v-data-table>
